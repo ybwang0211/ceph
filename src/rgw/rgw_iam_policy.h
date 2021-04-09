b17853a796546fbb518e42c2d005760ea7ec34b6
wangyingbin <ybwang0211@163.com>
2021-04-09 15:36:23 +0800

    doc: add "--foo" option to bar
    This commit updates the man page for bar with the newly added "--foo"
    option.
    
    Signed-off-by: wangyingbin <ybwang0211@163.com>

--- src/rgw/rgw_iam_policy.h
+++ src/rgw/rgw_iam_policy.h
@@ -1,5 +1,6 @@
 // -*- mode:C++; tab-width:8; c-basic-offset:2; indent-tabs-mode:t -*-
 // vim: ts=8 sw=2 smarttab ft=cpp
+//wangyingbin test
 
 #ifndef CEPH_RGW_IAM_POLICY_H
 #define CEPH_RGW_IAM_POLICY_H
